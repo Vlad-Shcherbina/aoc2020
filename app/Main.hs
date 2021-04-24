a237718c3d879b7ad229a1742214c10476a7048b
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2021-04-25 02:47:05 +0300

    Day 1, quick and dirty

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,17 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
+import System.IO
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    fin <- openFile "data/01.txt" ReadMode
+    c <- hGetContents fin
+    let xs :: [Int] = map read $ lines c
+
+    let result = [x * y | x <- xs, y <- xs, x + y == 2020]
+    putStrLn $ show result
+    
+    let result2 = [x * y * z | x <- xs, y <- xs, z <- xs, x + y + z == 2020]
+    putStrLn $ show result2
