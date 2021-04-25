648664fd67071e40967ed57413da526ad70b049c
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2021-04-25 17:04:48 +0300

    Move day 1 to a separate module

---
+++ app/Day01.hs
@@ -0,0 +1,12 @@
+module Day01 (day01) where
+
+import System.IO
+
+day01 :: IO ()
+day01 = do
+    fin <- openFile "data/01.txt" ReadMode
+    c <- hGetContents fin
+    let xs :: [Int] = map read $ lines c
+
+    putStrLn $ show $ head [x * y | x <- xs, y <- xs, x + y == 2020]    
+    putStrLn $ show $ head [x * y * z | x <- xs, y <- xs, z <- xs, x + y + z == 2020]
--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,7 @@ import System.IO
 import System.Environment
 import System.Exit
 
+import Day01 (day01)
 import Day02 (day02)
 
 main :: IO ()
@@ -15,12 +16,3 @@ main = do
         otherwise -> do
             putStrLn "Usage: cabal run aoc2020 -- <day>"
             exitWith $ ExitFailure 1
-
-day01 :: IO ()
-day01 = do
-    fin <- openFile "data/01.txt" ReadMode
-    c <- hGetContents fin
-    let xs :: [Int] = map read $ lines c
-
-    putStrLn $ show $ head [x * y | x <- xs, y <- xs, x + y == 2020]    
-    putStrLn $ show $ head [x * y * z | x <- xs, y <- xs, z <- xs, x + y + z == 2020]
