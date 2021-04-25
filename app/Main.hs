fe6520512275c2442a9e01414ec0181d02800ec5
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2021-04-25 14:07:58 +0300

    <day> command line arg

--- app/Main.hs
+++ app/Main.hs
@@ -3,9 +3,20 @@
 module Main where
 
 import System.IO
+import System.Environment
+import System.Exit
 
 main :: IO ()
 main = do
+    args <- getArgs
+    case args of
+        ["1"] -> day1
+        otherwise -> do
+            putStrLn "Usage: cabal run aoc2020 -- <day>"
+            exitWith $ ExitFailure 1
+
+day1 :: IO ()
+day1 = do
     fin <- openFile "data/01.txt" ReadMode
     c <- hGetContents fin
     let xs :: [Int] = map read $ lines c
