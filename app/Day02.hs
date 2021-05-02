bdc6b3cd1b6dbbacd95c23e6de06e2a1995e91d9
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2021-05-03 02:26:31 +0300

    Exhaustiveness check

--- app/Day02.hs
+++ app/Day02.hs
@@ -47,4 +47,5 @@ partition :: Text -> Text -> Maybe (Text, Text)
 partition sep s = case T.breakOnAll sep s of
     (left, right):_ -> case T.stripPrefix sep right of
         Just right -> Just (left, right)
+        Nothing -> undefined
     [] -> Nothing
