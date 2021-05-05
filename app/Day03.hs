838e60a535a5b82ce5b70ee82f0d553a1ee7577a
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2021-05-06 00:42:14 +0300

    Day 3, part 2

--- app/Day03.hs
+++ app/Day03.hs
@@ -1,16 +1,32 @@
 module Day03 (day03) where
 
 import System.IO
+import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 
-slopeRight = 3
+slopeRight = 3  -- part 1
+slopes = [1, 3, 5, 7]  -- part 2, except (1, 2)
 
 day03 :: IO ()
 day03 = withFile "data/03.txt" ReadMode \fin -> do
     c <- TIO.hGetContents fin
     let lines = T.lines c
 
-    let trace = [T.index line (i * slopeRight `mod` T.length line) | (i, line) <- zip [0..] lines]
     putStr "part 1: "
-    print $ length $ filter (== '#') trace
+    print $ treesOnTrace lines slopeRight
+
+    let treeCounts = treesOnTrace (everyOther lines) 1 :
+                     map (treesOnTrace lines) slopes
+    putStr "part 2: "
+    print $ product treeCounts
+
+treesOnTrace :: [Text] -> Int -> Int
+treesOnTrace lines slope =
+    let trace = [T.index line (i * slope `mod` T.length line) 
+                 | (i, line) <- zip [0..] lines]
+    in length $ filter (== '#') trace
+
+everyOther :: [a] -> [a]
+everyOther (x:y:xs) = x : everyOther xs
+everyOther xs = xs
