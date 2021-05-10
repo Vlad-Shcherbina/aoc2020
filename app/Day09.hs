70a01c860d9a2b96d8bac7dad45537f2a8827c2b
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2021-05-11 01:09:32 +0300

    Day 9, part 2

--- app/Day09.hs
+++ app/Day09.hs
@@ -1,7 +1,7 @@
 module Day09 (day09) where
 
 import System.IO
-import Data.List (tails)
+import Data.List (tails, scanl')
 import qualified Data.Set as S
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
@@ -14,9 +14,26 @@ day09 = withFile "data/09.txt" ReadMode \fin -> do
     let groups = [(take 25 xs, xs !! 25) | xs <- tails xs, length xs > 25]
 
     putStr "part 1: "
-    print $ head [s | (xs, s) <- groups, not $ isSum xs s]
+    let part1 = head [s | (xs, s) <- groups, not $ isSum xs s]
+    print part1
+
+    putStr "part 2: "
+    let [ys] = [ys | ys <- infixesWithSum xs part1, length ys > 1]
+    print $ minimum ys + maximum ys
 
 isSum :: [Int] -> Int -> Bool
 isSum xs sum = go S.empty xs where
     go s [] = False
     go s (x:xs) = (sum - x) `S.member` s || go (S.insert x s) xs
+
+prefixesWithSum :: [Int] -> Int -> [[Int]]
+prefixesWithSum xs sum = do
+    (len, s) <- scanl' (\(len, s) x -> (len + 1, s + x)) (0, 0) xs
+    if s == sum
+        then return $ take len xs
+        else []
+
+infixesWithSum :: [Int] -> Int -> [[Int]]
+infixesWithSum xs sum = do
+    t <- tails xs
+    prefixesWithSum t sum
