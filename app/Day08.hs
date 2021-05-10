5dcb24a82f3e47b7c67104c5e96fbc259dad2fe8
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2021-05-10 23:35:21 +0300

    Day 8, part 2

--- app/Day08.hs
+++ app/Day08.hs
@@ -12,20 +12,39 @@ day08 :: IO ()
 day08 = withFile "data/08.txt" ReadMode \fin -> do
     c <- TIO.hGetContents fin
     let prog = map parseInsn $ T.lines c
-    putStr "part 1: "
-    print $ part1 prog
 
-part1 :: [Insn] -> Int
-part1 prog = loop S.empty 0 0 where
-    loop visited ip acc = if ip `S.member` visited
-        then acc
+    let Loop part1 = run prog
+    putStr "part 1: "
+    print part1
+
+    let [part2] = [x | Finished x <- map run $ variations prog]
+    putStr "part 2: "
+    print part2
+
+variations :: [Insn] -> [[Insn]]
+variations prog = do
+    (i, insn) <- zip [0..] prog
+    alternative <- case insn of
+        Nop x -> [Jmp x]
+        Jmp x -> [Nop x]
+        Acc _ -> []
+    return $ take i prog ++ [alternative] ++ drop (i + 1) prog
+
+data RunResult = Loop Int | Finished Int deriving (Show)
+
+run :: [Insn] -> RunResult
+run prog = rec S.empty 0 0 where
+    rec visited ip acc = if ip `S.member` visited
+        then Loop acc
         else let
             visited' = S.insert ip visited
             ip' = ip + 1
-            in case prog !! ip of
-                Nop _ -> loop visited' ip' acc
-                Acc x -> loop visited' ip' (acc + x)
-                Jmp x -> loop visited' (ip + x) acc
+            in if ip == length prog
+                then Finished acc
+                else case prog !! ip of
+                    Nop _ -> rec visited' ip' acc
+                    Acc x -> rec visited' ip' (acc + x)
+                    Jmp x -> rec visited' (ip + x) acc
 
 data Insn
     = Nop Int
