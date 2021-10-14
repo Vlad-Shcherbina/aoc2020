ce88ff20e44a40f4c06aeb663acf16eb0850fc1d
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2021-10-14 22:45:04 +0000

    Day 14, part 2

--- app/Day14.hs
+++ app/Day14.hs
@@ -6,7 +6,7 @@ import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import qualified Data.Map.Strict as Map
 import Data.Int ( Int64 )
-import Data.List ( foldl' )
+import Data.List ( foldl', unfoldr )
 import Data.Bits
 import Util ( partition )
 
@@ -16,19 +16,32 @@ day14 = withFile "data/14.txt" ReadMode \fin -> do
     let lines = T.lines c
     let insns = map parse lines
     
-    let initState = State { mask = Mask { ones = 0, zeros = 0 }, mem = Map.empty}
-    let State { mem } = foldl' applyInsn initState insns
+    let solve applyInsn = let
+            initState = State { mask = Mask { ones = 0, zeros = 0 }, mem = Map.empty}
+            State { mem } = foldl' applyInsn initState insns
+            in sum $ Map.elems mem
     
     putStr "part 1: "
-    print $ sum $ Map.elems mem
+    print $ solve applyInsn
+    putStr "part 2: "
+    print $ solve applyInsn2
 
-data State = State { mask :: Mask, mem :: Map.Map Int Int64 } deriving (Show)
+data State = State { mask :: Mask, mem :: Map.Map Int64 Int64 } deriving (Show)
+
 applyInsn :: State -> Insn -> State
 applyInsn state (SetMask newMask) = state { mask = newMask }
 applyInsn State { .. } WriteMem { .. } = State {
     mask,
     mem = Map.insert addr (applyMask mask value) mem
     }
+
+applyInsn2 :: State -> Insn -> State
+applyInsn2 state (SetMask newMask) = state { mask = newMask }
+applyInsn2 State { .. } WriteMem { .. } = State {
+    mask,
+    mem = foldl' f mem (floatMask (combineMask mask addr))
+    } where
+        f mem addr = Map.insert addr value mem
 
 data Mask = Mask { ones :: Int64, zeros :: Int64 } deriving (Show)
 
@@ -43,9 +56,18 @@ parseMask s = assert (T.length s == 36) $
 applyMask :: Mask -> Int64 -> Int64
 applyMask Mask {..} x = x .&. zeros .|. ones
 
+combineMask :: Mask -> Int64 -> Mask
+combineMask Mask {..} addr = Mask {
+    ones = ones .|. addr .&. complement zeros,
+    zeros = zeros .|. addr
+}
+
+floatMask :: Mask -> [Int64]
+floatMask Mask {..} = map (ones .|.) (bitPowerset (zeros .&. complement ones))
+
 data Insn
     = SetMask Mask
-    | WriteMem { addr :: Int, value :: Int64 }
+    | WriteMem { addr :: Int64, value :: Int64 }
     deriving (Show)
 
 parse :: T.Text -> Insn
@@ -64,3 +86,9 @@ parseBinary = T.foldl' op 0 where
         '0' -> 0
         '1' -> 1
         _ -> undefined
+
+-- https://www.chessprogramming.org/Traversing_Subsets_of_a_Set#All_Subsets_of_any_Set
+bitPowerset :: Int64 -> [Int64]
+bitPowerset mask = loop 0 where
+    loop x = x : (if y == 0 then [] else loop y) where
+        y = (x - mask) .&. mask
