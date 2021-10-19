1622e314462b1fa84050e027fe78491478bfacc6
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2021-10-19 13:00:51 +0300

    Day 16, part 2

--- app/Day16.hs
+++ app/Day16.hs
@@ -3,6 +3,8 @@ module Day16 (day16) where
 import System.IO
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
+import Data.List (transpose)
+import Data.List.Extra (minimumOn)
 import Util (partition)
 
 day16 :: IO ()
@@ -17,10 +19,30 @@ day16 = withFile "data/16.txt" ReadMode \fin -> do
 
     let allRanges = concatMap ranges rules
     let allFields = concat nearby
-    let isValid field = any (\(l, r) -> l <= field && field <= r) allRanges
+    let isValid = inRanges allRanges
 
-    putStr "part 1:"
+    putStr "part 1: "
     print $ sum $ filter (not . isValid) allFields
+
+    let validNearby = filter (all isValid) nearby
+    let columns = transpose validNearby
+    let matchingRules column = [name | Rule { name, ranges } <- rules, all (inRanges ranges) column]
+    let [sol] = solveConstraints $ zip [0..] (map matchingRules columns)
+
+    putStr "part 2: "
+    print $ product [myTicket !! i | (i, name) <- sol, T.isPrefixOf "departure" name]
+
+solveConstraints :: (Eq k, Eq a) => [(k, [a])] -> [[(k, a)]]
+solveConstraints [] = pure []
+solveConstraints constraints = do
+    let (k, as) = minimumOn (\(k, as) -> length as) constraints
+    a <- as
+    let constraints' = [(k', filter (/= a) as') | (k', as') <- constraints, k' /= k]
+    sol <- solveConstraints constraints'
+    pure $ (k, a) : sol
+
+inRanges :: [(Int, Int)] -> Int -> Bool
+inRanges ranges x = or [l <= x && x <= r | (l, r) <- ranges]
 
 parseTicket :: T.Text -> [Int]
 parseTicket s = map (read . T.unpack) $ T.split (==',') s
